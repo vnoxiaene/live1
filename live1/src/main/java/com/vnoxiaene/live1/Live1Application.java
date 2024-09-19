f196c4d10adc4b12422f7a14a2447f036ca6dbb3
Daniel Lima <dan.musik@gmail.com>
2024-09-19 18:23:31 -0300

    first commit

---
+++ live1/src/main/java/com/vnoxiaene/live1/Live1Application.java
@@ -0,0 +1,13 @@
+package com.vnoxiaene.live1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Live1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Live1Application.class, args);
+	}
+
+}
